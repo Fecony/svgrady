1a5b145d1dfe5f16b35d3d522004749753fa23fa
Ričards Tagils <tagils@mail.ru>
2020-02-23 00:31:37 +0200

    Rollup config + babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('da' + 'da')
+class SVGRady {
+  constructor (startAngle, endAngle, activeColor, color) {
+    this.startAngle = startAngle
+    this.endAngle = endAngle
+    this.activeColor = activeColor
+    this.color = color
+  }
+}
+
+export default SVGRady
