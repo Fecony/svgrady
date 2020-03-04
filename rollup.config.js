7d6eebc6b8931113dff8331ce5a57ccff48fbf90
Ričards Tagils <tagils@mail.ru>
2020-03-04 22:11:15 +0200

    remove sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
-import {
-  main,
-  module,
-  browser,
-  dependencies,
-  peerDependencies
-} from './package.json'
+import { main, module, browser, dependencies, peerDependencies } from './package.json'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -17,17 +11,14 @@ export default {
   output: [
     {
       file: main,
-      sourcemap: true,
       format: 'cjs'
     },
     {
       file: module,
-      sourcemap: true,
       format: 'esm'
     },
     {
       name: 'SVGrady',
-      sourcemap: true,
       file: browser,
       format: 'umd'
     }
@@ -36,10 +27,7 @@ export default {
     include: ['src/**'],
     exclude: ['node_modules/**']
   },
-  external: [
-    ...Object.keys(dependencies || {}),
-    ...Object.keys(peerDependencies || {})
-  ],
+  external: [...Object.keys(dependencies || {}), ...Object.keys(peerDependencies || {})],
   plugins: [
     resolve(),
     typescript({
